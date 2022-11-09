8fcba9799577fb99a49b71f0f91caf5e88617522
Oscar Flores <59371855+CocaFlores@users.noreply.github.com>
2022-11-08 19:28:48 -0600

    Update TarjetaPresentacion.java

--- TarjetaPresentacion.java
+++ TarjetaPresentacion.java
@@ -1,3 +1,41 @@
 public class TarjetaPresentacion {
+    String nombre;
+    String trabajo;
+    int telefono;
+    String correo;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTrabajo() {
+        return this.trabajo;
+    }
+
+    public void setTrabajo(String trabajo) {
+        this.trabajo = trabajo;
+    }
+
+    public int getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return this.correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+
     
 }
