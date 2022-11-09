9cc862e46efa55e667664e0f0bee18d0078b6ad7
ArielaMishaan <109829847+ArielaMishaan@users.noreply.github.com>
2022-11-09 16:58:00 -0600

    métodos de interface

--- Radio.java
+++ Radio.java
@@ -6,6 +6,114 @@
  * Clase que modela la Radio. Es la controladora.
  */
 
-public class Radio {
+public class Radio implements InterfaceB{
+
+    @Override
+    public String cambiarVolumen(int subirBajar) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String cambiarRadio() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String cambiarEmisora(int subirBajar) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void guardarEmisora() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String cargarEmisora(int numeroEmisora) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String desplegarEmisoras() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String desplegarListas() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String cambiarModo(int modo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String seleccionarLista(int lista) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String cambiarCancion(int subirBajar) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String escucharCancion(int cancion) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String conectar(int numeroTelefono) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String desplegarCanciones() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String desplegarContactos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String encenderApagar() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String llamarColgar(int opcion, int contacto) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String desplegarTarjetas() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String desconectar() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
