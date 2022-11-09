fd0f52a4f9658fc569859ae5c213acfc1d2de087
Oscar Flores <59371855+CocaFlores@users.noreply.github.com>
2022-11-08 19:48:18 -0600

    Update TarjetaPresentacion.java

--- TarjetaPresentacion.java
+++ TarjetaPresentacion.java
@@ -7,6 +7,20 @@ public class TarjetaPresentacion {
     public TarjetaPresentacion() {
     }
 
+    
+    /** 
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "{" +
+            " nombre='" + getNombre() + "'" +
+            ", trabajo='" + getTrabajo() + "'" +
+            ", telefono='" + getTelefono() + "'" +
+            ", correo='" + getCorreo() + "'" +
+            "}";
+    }
+
     public TarjetaPresentacion(String nombre, String trabajo, int telefono, String correo) {
         this.nombre = nombre;
         this.trabajo = trabajo;
@@ -14,47 +28,70 @@ public class TarjetaPresentacion {
         this.correo = correo;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getNombre() {
         return this.nombre;
     }
 
+    
+    /** 
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getTrabajo() {
         return this.trabajo;
     }
 
+    
+    /** 
+     * @param trabajo
+     */
     public void setTrabajo(String trabajo) {
         this.trabajo = trabajo;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getTelefono() {
         return this.telefono;
     }
 
+    
+    /** 
+     * @param telefono
+     */
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getCorreo() {
         return this.correo;
     }
 
+    
+    /** 
+     * @param correo
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
-    @Override
-    public String toString() {
-        return "{" +
-            "\nNombre: " + getNombre() + "\n" +
-            "Trabajo: " + getTrabajo() + "\n" +
-            "Telefono: " + getTelefono() + "'" +
-            "Correo: " + getCorreo() + "\n" +
-            "";
-    }
 
     
 }
