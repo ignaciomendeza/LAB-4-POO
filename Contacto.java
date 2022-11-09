d2e4becb8fd42c0addcffdc38e40d329157262c6
Oscar Flores <59371855+CocaFlores@users.noreply.github.com>
2022-11-08 19:22:29 -0600

    Update Contacto.java

--- Contacto.java
+++ Contacto.java
@@ -1,3 +1,20 @@
 public class Contacto {
-    
+    String nombre;
+    int telefono;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
