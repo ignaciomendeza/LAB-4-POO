256fa4ae81ba715875fb13a276338428fbec2a3a
Oscar Flores <59371855+CocaFlores@users.noreply.github.com>
2022-11-08 19:26:59 -0600

    Update Cancion.java

--- Cancion.java
+++ Cancion.java
@@ -1,3 +1,41 @@
 public class Cancion {
+    String cantante;
+    float duracion;
+    String nombre;
+    String genero;
+
+    public String getCantante() {
+        return this.cantante;
+    }
+
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+
+    public float getDuracion() {
+        return this.duracion;
+    }
+
+    public void setDuracion(float duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
     
 }
+
