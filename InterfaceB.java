6f3125ed3f6b2a9260d003c54e9e2cf1c52fb22f
ignaciomendeza <109829839+ignaciomendeza@users.noreply.github.com>
2022-11-08 18:57:17 -0600

    Update InterfaceB.java
    Creación de métodos de la interface

--- InterfaceB.java
+++ InterfaceB.java
@@ -1,3 +1,21 @@
 public interface InterfaceB{
-    
+    public String cambiarVolumen(int subirBajar);
+    public String cambiarRadio();
+    public String cambiarEmisora(int subirBajar);
+    public void guardarEmisora();
+    public String cargarEmisora(int numeroEmisora);
+    public String desplegarEmisoras();
+    public String desplegarListas();
+    public String cambiarModo(int modo);
+    public String seleccionarLista(int lista);
+    public String cambiarCancion(int subirBajar);
+    public String escucharCancion(int cancion);
+    public String conectar(int numeroTelefono);
+    public String desplegarCanciones();
+    public String desplegarContactos();
+    public String encenderApagar();
+    public String llamarColgar(int opcion, int contacto);
+    public String desplegarTarjetas();
+    public String desconectar();
+    public String toString();
 }
